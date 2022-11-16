d5b031efa9d44384b8c42420d44b125f4662456b
Anthony Cherbuin <anthony.cherbuin@bold.ch>
2022-11-16 13:04:13 +0100

    change srv url

--- components/Mobile.vue
+++ components/Mobile.vue
@@ -29,7 +29,7 @@ export default {
     };
   },
   mounted() {
-    this.socket = io("https://websocket-nuxt3.vercel.app:4000");
+    this.socket = io("https://websocket-nuxt3-connection.vercel.app:4000");
 
     // Connect to socker io server
     this.socket.on("connect", () => {
